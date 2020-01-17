9c9abeafdfa8933ed70f8b9a962ee96c5516f8ba
Thiago Nogueira <thiagonf10@gmail.com>
2020-01-17 14:02:24 -0300

    Adding configuration graphql in settings

--- project/backend/settings.py
+++ project/backend/settings.py
@@ -20,6 +20,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
+    'api_proxy',
 ]
 
 MIDDLEWARE = [
@@ -107,3 +109,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = config('STATIC_URL', default='/static/')
+
+
+GRAPHENE = {
+    'SCHEMA': 'backend.schema.schema',
+}

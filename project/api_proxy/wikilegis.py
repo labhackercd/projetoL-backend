ade0a38b2554f969ecd6d14bf18cb797838307d4
Thiago Nogueira <thiagonf10@gmail.com>
2020-01-17 15:31:43 -0300

    Adding suggetion model and querry resolve methods

--- project/api_proxy/wikilegis.py
+++ project/api_proxy/wikilegis.py
@@ -33,15 +33,57 @@ class User(ObjectType):
     profile = Field(Profile)
 
 
+class Suggestion(ObjectType):
+    id = ID()
+    created = DateTime()
+    modified = DateTime()
+    excerpt = String()
+    start_index = Int()
+    end_index = Int()
+    content = String()
+    author = Field(User)
+
+
 class Query(ObjectType):
     users = List(User)
+    suggestions = List(Suggestion, idSugestion=Int(
+        required=False), idUser=Int(required=False))
 
     def resolve_users(self, info, **kwargs):
-        users = api_call()["results"]
+        users = api_call_user()["results"]
+        return json2obj(dumps(users))
+
+    def resolve_suggestions(self, info, idSugestion=None, idUser=None):
+        if idSugestion:
+            users = api_call_sugestion(idSugestion)["results"]
+        elif idUser:
+            users = api_call_sugestions_user(idUser)["results"]
+        else:
+            users = api_call_sugestions()["results"]
         return json2obj(dumps(users))
 
 
-def api_call():
+def api_call_user():
     response = requests.get('https://edemocracia.camara.leg.br/wikilegis/api/v1/users/',
                             headers={'Accept': 'application/json'})
+    return response.json()
+
+
+def api_call_sugestions():
+    response = requests.get('https://edemocracia.camara.leg.br/wikilegis/api/v1/sugestions/',
+                            headers={'Accept': 'application/json'})
+    return response.json()
+
+
+def api_call_sugestion(id: Int):
+    url_format = 'https://edemocracia.camara.leg.br/wikilegis/api/v1/sugestions/?id={}'.format(
+        id)
+    response = requests.get(url_format, headers={'Accept': 'application/json'})
+    return response.json()
+
+
+def api_call_sugestions_user(id: Int):
+    url_format = 'https://edemocracia.camara.leg.br/wikilegis/api/v1/sugestions/?author__id={}'.format(
+        id)
+    response = requests.get(url_format, headers={'Accept': 'application/json'})
     return response.json()
